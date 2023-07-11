1a22c406b5a59421569b61fbfea4955a4c61e568
Bernardo Campos <bernardocjs@gmail.com>
2023-07-10 22:22:58 -0300

    feat: :sparkles: adds register user  use-case

--- src/http/controllers/register.controler.ts
+++ src/http/controllers/register.controler.ts
@@ -12,28 +12,7 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
   const { name, email, password } = registerBodySchema.parse(request.body);
 
-  const password_hash = await hash(password, 6);
 
-  const userWithSameEmail = await prisma.user.findUnique({
-    where: {
-      email,
-    },
-  });
-
-  if (userWithSameEmail) {
-    reply.status(409).send({
-      error: 'User with same email already exists',
-    });
-    return;
-  }
-
-  await prisma.user.create({
-    data: {
-      name,
-      email,
-      password_hash
-    },
-  });
 
   reply.status(201).send();
 }
