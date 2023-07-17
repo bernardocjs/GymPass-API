ee78463a5b7a08459bfbe40f753ed72b88c8110e
Bernardo Campos <bernardocjs@gmail.com>
2023-07-17 18:03:54 -0300

    feat: :sparkles: adds auth controller

---
+++ src/http/controllers/authenticate.controller.ts
@@ -0,0 +1,31 @@
+import { z } from 'zod';
+import { FastifyRequest, FastifyReply } from 'fastify';
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository';
+import { AuthenticateUseCase } from '@/use-cases/authenticate';
+import { InvalidCredentialsError } from '@/use-cases/errors/invalid-credentials-error';
+
+export async function authenticate(request: FastifyRequest, reply: FastifyReply) {
+  const registerBodySchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(4),
+  });
+
+  const {  email, password } = registerBodySchema.parse(request.body);
+
+  try {
+    const usersRepository = new PrismaUsersRepository();
+    const authenticateUseCase = new AuthenticateUseCase(usersRepository);
+
+    await authenticateUseCase.execute({
+      email,
+      password,
+    });
+  } catch (err) {
+    if (err instanceof InvalidCredentialsError)
+      return reply.status(400).send({ message: err.message });
+
+    throw err;
+  }
+
+  reply.status(200).send();
+}
