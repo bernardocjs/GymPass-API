53d6ea8e5a2dfc135093ed27b9668160e0a8425d
Bernardocjs <bernardocjs@gmail.com>
2024-10-02 13:02:11 -0300

    fix: :sparkles: adds report

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,12 @@
-import {defineConfig} from 'vitest/config';
+import { defineConfig } from 'vitest/config';
 import tsconfigPaths from 'vite-tsconfig-paths';
+
 export default defineConfig({
   plugins: [tsconfigPaths()],
+  test: {
+    reporters: ['default', 'html'],
+    outputFile: {
+      html: './reports/test-report.html',
+    },
+  },
 });
